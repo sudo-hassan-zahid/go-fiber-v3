f57c8f5f01e75dacadabb83802cf7bda089d820b
Hassan <hassanisavailable@gmail.com>
2026-05-17 07:51:07 +0500

    updated fiber version and added the logger

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+)
 
 func main() {
 	app := fiber.New(fiber.Config{
 		AppName: "API Server",
 	})
+
+	app.Use(logger.New(logger.Config{
+		Format:      "[${time}] ${status} - ${method} ${path}\n",
+		ForceColors: true,
+	}))
 
 	if err := app.Listen(":8080"); err != nil {
 		panic(err)
