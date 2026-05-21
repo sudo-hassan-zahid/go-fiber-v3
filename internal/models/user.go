d8fdd8941d51c46c7bb005781c3f17fe5062d1f3
Hassan <hassanisavailable@gmail.com>
2026-05-21 10:19:24 +0500

    Add user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,9 @@
 package models
+
+type User struct {
+	BaseModel
+	FirstName string `json:"first_name" gorm:"type:varchar(100);not null"`
+	LastName  string `json:"last_name" gorm:"type:varchar(100);not null"`
+	Email     string `json:"email" gorm:"type:varchar(100);not null;uniqueIndex"`
+	Password  string `json:"password" gorm:"type:varchar(100);not null"`
+}
